14898ddcda97d3952996225e21f037291b48078f
Seline <seline@binus.edu>
2023-02-08 23:27:46 +0700

    adjust language & error message

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -81,6 +81,16 @@
             </div>
         </div>
 
+        @if ($errors->any())
+            <div class="alert alert-danger mt-3">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <div class="d-flex flex-column justify-content-center mt-3">
             <button type="submit" class="btn btn-primary">{{__("message.Save")}}</button>
         </div>
